55b8781d31a038fe4b2e3df20660ae1b3aa92449
unknown <idyllistoic@gmail.com>
2024-02-17 15:41:49 +0100

    added save and load model

--- predictor.py
+++ predictor.py
@@ -1,7 +1,8 @@
+import os
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Dense, LSTM, TimeDistributed, Embedding, Bidirectional
 from tensorflow.keras import layers
 
@@ -9,7 +10,8 @@ tf.keras.utils.set_random_seed(812)
 
 pd.set_option('future.no_silent_downcasting', True)
 
-class PSSPredictor():
+# @tf.keras.utils.register_keras_serializable(package='Custom', name='PSSPredictor')
+class PSSPredictor:
     def __init__(self, window_size, protein_letters='ACDEFGHIKLMNPQRSTVWXY', secondary_letters='ceh'):
         self.window_size = window_size
         self.protein_letters = protein_letters
@@ -36,21 +38,35 @@ class PSSPredictor():
             TimeDistributed(Dense(len(self.secondary_letters), activation='softmax')) # Apply Dense layer to each time step
         ])
 
-        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy', 'mae', self.q3_score])
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy', 'mae', q3_score])
 
         return model
 
     def train(self, X_train, y_train, X_val, y_val, epochs=100, batch_size=32, validation_split=0.2):
-        self.model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=validation_split)
-        loss, accuracy, mae, q3 = self.model.evaluate(X_val, y_val)
-        print(f'Validation Loss: {loss}, Accuracy: {accuracy}, MAE: {mae}, Q3: {q3}')
+        if not os.path.exists("model.keras"):
+            self.model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=validation_split)
+            self.model.save("model.keras")
+            loss, accuracy, mae, q3 = self.model.evaluate(X_val, y_val)
+            print(f'Validation Loss: {loss}, Accuracy: {accuracy}, MAE: {mae}, Q3: {q3}')
+        else:
+            self.model = self.load_model('model.keras')
+       
     
     def predict(self, X):
         return self.model.predict(X)
     
-    def q3_score(self, target, prediction):
-        target_labels = tf.argmax(target, axis=-1)
-        prediction_labels = tf.argmax(prediction, axis=-1)
-        q3 = tf.reduce_mean(tf.cast(tf.equal(target_labels, prediction_labels), dtype=tf.float32))
-        return q3
+    @staticmethod
+    def load_model(filepath='model.keras'):
+        # Load the model from the file
+        loaded_model = tf.keras.models.load_model(filepath, custom_objects={"q3_score": q3_score})
+        print("Model loaded successfully.")
+        return loaded_model
 
+
+@tf.keras.utils.register_keras_serializable(package="Custom", name="q3_score")
+def q3_score(target, prediction):
+    target_labels = tf.argmax(target, axis=-1)
+    prediction_labels = tf.argmax(prediction, axis=-1)
+    q3 = tf.reduce_mean(tf.cast(tf.equal(target_labels, prediction_labels), dtype=tf.float32))
+    return q3
+    
