a9d6b666c5c8dca403e2ec72c1a494cfc310d4e6
unknown <idyllistoic@gmail.com>
2024-02-17 14:49:43 +0100

    added q3

--- predictor.py
+++ predictor.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
-import tensorflow
+import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM, TimeDistributed, Embedding, Bidirectional
 from tensorflow.keras import layers
 
-tensorflow.keras.utils.set_random_seed(812)
+tf.keras.utils.set_random_seed(812)
 
 pd.set_option('future.no_silent_downcasting', True)
 
@@ -36,14 +36,21 @@ class PSSPredictor():
             TimeDistributed(Dense(len(self.secondary_letters), activation='softmax')) # Apply Dense layer to each time step
         ])
 
-        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy', 'mae'])
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy', 'mae', self.q3_score])
 
         return model
 
     def train(self, X_train, y_train, X_val, y_val, epochs=100, batch_size=32, validation_split=0.2):
         self.model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=validation_split)
-        loss, accuracy, mae = self.model.evaluate(X_val, y_val)
-        print(f'Validation Loss: {loss}, Accuracy: {accuracy}, MAE: {mae}')
+        loss, accuracy, mae, q3 = self.model.evaluate(X_val, y_val)
+        print(f'Validation Loss: {loss}, Accuracy: {accuracy}, MAE: {mae}, Q3: {q3}')
     
     def predict(self, X):
         return self.model.predict(X)
+    
+    def q3_score(self, target, prediction):
+        target_labels = tf.argmax(target, axis=-1)
+        prediction_labels = tf.argmax(prediction, axis=-1)
+        q3 = tf.reduce_mean(tf.cast(tf.equal(target_labels, prediction_labels), dtype=tf.float32))
+        return q3
+
